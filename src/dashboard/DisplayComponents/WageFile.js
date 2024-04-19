ae1f6fdabda391a4b1701ed23fb272ead4f5c9c3
Ramesh-krishnan-v <rameshram7401@gmail.com>
2024-04-19 09:50:48 +0530

    Added witth DA

--- src/dashboard/DisplayComponents/WageFile.js
+++ src/dashboard/DisplayComponents/WageFile.js
@@ -170,7 +170,7 @@ export default function WageFile({ wageData, dates, index }) {
                                                         </SubTitleGrid>
                                                         <Grid item xs={6} style={{ border: '1px solid black', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                                                             <Typography variant="subtitle1">
-                                                                {Math.round(wageData?.DA * 100) / 100 ?? ""}
+                                                                {wageData?.DA ? Math.round(wageData?.DA * 100) / 100 : ""}
                                                             </Typography>
                                                         </Grid>
                                                         <Grid item xs={1} style={{ border: '1px solid black', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
